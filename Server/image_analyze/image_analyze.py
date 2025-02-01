e54fb1bb30e46654ec2aa31c9a6315978b7f7941
KimSeongRyeol <kab6440@naver.com>
2025-02-01 21:44:10 +0900

    이미지 분석 함수 추가

--- Server/image_analyze/image_analyze.py
+++ Server/image_analyze/image_analyze.py
@@ -16,3 +16,28 @@ os.makedirs(SAVE_DIR, exist_ok=True)
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect(BROKER_ADDRESS, PORT, 60)
 client.loop_start()
+
+model = YOLO("yolo11n.pt")
+
+# 이미지 분석 함수
+def process_image(image_path):
+    image = cv2.imread(image_path)
+    if image is None:
+        print(f"이미지를 로드할 수 없습니다: {image_path}")
+        return None
+
+    results = model(image)
+    detected_objects = []
+
+    for result in results[0].boxes:
+        x1, y1, x2, y2 = map(int, result.xyxy[0])
+        conf = result.conf[0].item()
+        cls = int(result.cls[0])
+        label = model.names[cls]
+
+        detected_objects.append({
+            "object": label,
+            "confidence": round(conf, 2)
+        })
+
+    return detected_objects
